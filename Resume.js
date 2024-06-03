8714b1a8f430e3925d560be5dcf7c0c0981c4b10
Olivia Criscione <oliviagcriscione@gmail.com>
2024-06-03 10:14:25 -0400

    Create Resume.js

---
+++ Resume.js
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta http-equiv="Content-Type">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+    <link rel="stylesheet" href="Resume.css">
+    <script src="Resume.js" defer></script>
+</head>
+
+<body>
+<div class="sticky_contactinfo" id="stickyHeader">
+    <button class="darkmodebutton" onclick="toggleDarkMode()">Dark mode</button>
+    <h1>Olivia G. Criscione</h1>
+    <h4>(518) 390-0888</h4>
+    <div class="button-container">
+        <a class="linkedin-button" href="https://www.linkedin.com/in/olivia-criscione-ab54301b0/">LinkedIn</a>
+    </div>
+    <div class="button-container">
+        <a class="email-button" href="/cdn-cgi/l/email-protection#711e1d18071810161203180212181e1f1431161c10181d5f121e1c">Email Me</a>
+    </div>
+</div>
+
+<hr>
+<div class="Education">
+    <h2>Education</h2>
+    <hr>
+    <p>University at Albany (SUNY) | GPA 4.0</p>
+    <p2>B.S. in Cyber Security - August 2024</p2>
+    <br>
+    <p3 id="graduation-timer">Graduation Timer</p3>
+    <ul>
+        <li>Honors: Dean's List - Fall 2023, SUNY Albany Merit Scholarship Recipient</li>
+        <li>Organizations: the National Society of Leadership and Success Society, Tau Sigma National Honor Society, Cyber Defense Organization, Epsilon Delta Psi</li>
+    </ul>
+    <p>Hudson Valley Community College | GPA 3.9</p>
+    <p2>A.S. in Hacking and Cyber Security - December 2023</p2>
+    <ul>
+        <li>Honors: President's List - Fall 2022, Spring 2023</li>
+        <li>Member of Phi Theta Kappa Honor Society</li>
+        <li>Participant of the Forensic Investigative Curriculum Program with the New York State Police</li>
+    </ul>
+   <hr>
+<div class="Experience">
+<h2>Experience</h2>
+<hr>
+<p>Student Assistant | New York State Information Technology Services | Albany NY | February 2024 - Present</p>
+<ul>
+    <li>Provide analysis, planning, and execution of recovery testing based on newly created and existing recovery plans within the Disaster Recovery Support team.</li>
+    <li>Create and maintain ITS Disaster Recovery documentation for ITS core services and critical applications.</li>
+    <li>Analyze and evaluate the service owner's written material and system specifications of the systems covered by the DR plan.</li>
+    <li>Facilitate meetings attended by project team(s) created based on the system analyzed, ITS management, and CTO staff to discuss specific DR plans and tests.</li>
+    <li>Analyze the architecture of critical systems and core services to ensure continuity of essential agency functions for ITS-supported executive agencies to ensure project success.</li>
+    <li>Participate in the development, interpretation, review, and communication of Disaster Recovery policies, procedures, and National Institute of Standards and Technology (NIST) standards to ensure Disaster Recovery documentation is consistent and standards are met.</li>
+</ul>
+<p>Mobile Expert | T-Mobile, Albany NY | November 2023 - June 2024</p>
+<ul>
+	<li>Cellular Sales (Voice lines and beyond the smart phone lines) as well as Home Internet Sales.</li>
+    <li>Update all work tablets to latest software as well as ensure that Zscaler is updated and working properly on all work related devices.</li>
+    <li>Provide device troubleshooting for customers experiencing issues with their devices or with their cellular network.</li>
+    <li>Provide network setup for T-Mobile Internet Customers.</li>
+    <li>Device data transfers.</li>
+    <li>March 2024, April 2024 Power Ranker MVP</li>
+ </ul>  
+<p>Drone Lab Intern | University at Albany (SUNY) | January 2024 - May 2024</p>
+<ul>
+	<li>Teach visitors how to operate the drones and robots that we have in the lab.</li>
+    <li>Run events with high schools and other college classes, teaching them about the technology we use and how to operate them.</li>
+</ul>
+<p>Peer Tutor | Hudson Valley Community College Learning Assistance Center, Troy NY | February 2023 - December 2023</p>
+<ul>
+	<li>Mathematics peer tutor for students taking MATH 190 and below.</li>
+    <li>Cyber Security / Computer Information Systems peer tutor.</li>
+    <li>Assist students with their assignments as well as studying for exams for mathematics, programming, and cyber security courses.</li>
+    <li>Final exam preparation for students.</li>
+</ul>
+<p>Information Technology Assistant | JLC Realty, Albany NY | March 2023 - December 2023</p>
+<ul>
+	<li>Setup and maintained company network, ensuring seamless connectivity.</li>
+	<li>Configured printer settings, troubleshooted printing/fax machine issues.</li>
+	<li>Provided technical support for computer setup and smartphone assistance.</li>
+<ul>
+
+</div>
+
+
+<hr>
+<div class="Skills">
+<h2>Skills</h2>
+<hr>
+<ul>
+	<li>Java Programming Language</li>
+	<li><a href="https://www.albany.edu/~oc773869/OCResume/Resume">HTML / Java Script</a></li> 
+	<li><a href="https://www.albany.edu/~oc773869/OCResume/Database%20Final%20CINF%20202%20-%20Olivia%20Criscione.pdf">MySQL</a>: ERD Design (Physical + Logical). Scripting with entities, attributes, cardinality, primary and foreign keys, and data types. Queries: operators, clauses, functions, and subqueries.</li>
+	<li>Python Programming Language</li>
+	<li>Ubuntu Linux Operating System Language</li>
+	<li>MATLAB Programming platform</li>
+	<li>Cisco Networking</li> 
+	<li>Information Security: 
+		CIA Triad, 
+		<a href="https://www.albany.edu/~oc773869/OCResume/Final%20Paper%20-%20CINF100X%20-%20Olivia%20Criscione.pdf">IoT Security Vulnerabilities</a>, 
+		<a href="https://www.albany.edu/~oc773869/OCResume/Final%20Paper%20-%20CINF%20200%20-%20Olivia%20Criscione.pdf">IoT Security Mitigation Strategies</a>, 
+		<a href="https://www.albany.edu/~oc773869/OCResume/Security%20Policy%20Shell%20-%20Olivia%20Criscione.pdf">Security Shell </a>, 
+		<a href="https://www.albany.edu/~oc773869/OCResume/Final%20Paper%20CYBR%20306%20-%20Olivia%20Criscione.pdf">Risk Management Plan</a>, 
+		<a href="https://www.albany.edu/~oc773869/OCResume/Olivia%20Criscione%20-%20CYBR%20306%20-%20Security%20Tools.pdf">Security Tools</a>, 
+		and <a href="https://www.albany.edu/~oc773869/OCResume/Privacy%20Assignment%20CINF%20306.pdf">Privacy</a>.</li>
+	<li>Calculus: Limits, Derivatives, Integrals, Series and Sequences, Polar Coordinates, Related Rates, Optimization.</li>
+	<li>Computer-Aided Design: 2D and 3D</li>
+	<li>Technical Writing</li>
+</ul>
+</div>
+
+<hr>
+<a class="download-button" href="Resume.pdf" download>Download Resume PDF</a>
+<hr>
+</body>
+</html>
